41c275f9ffc2c1953a20c117b393bdc3535d79fb
TENGO <tengogithub@gmail.com>
2026-02-19 22:16:20 +0400

    check

---
+++ main.js
@@ -0,0 +1,16 @@
+const body=document.body
+const openBtn = document.getElementById('burger-btn')
+const closBtn = document.getElementById('close-btn')
+const burgerMenu =document.getElementById('burger-menu')
+
+
+
+function burger(){
+    burgerMenu.classList.toggle('show-burger-menu')
+   closBtn.classList.toggle('show-close-btn')
+   openBtn.classList.toggle('hide-burger-btn')
+   body.classList.toggle('no-scroll')
+   burgerMenu.classList.toggle('scroll')
+}
+openBtn.addEventListener('click',burger)
+closBtn.addEventListener('click',burger)
